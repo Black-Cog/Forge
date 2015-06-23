2ca2ea6057e86216d7284e74566985331449d127
Black-Cog <blackcogdigital@gmail.com>
2015-06-23 18:47:49 +0200

    Added Maya and Nuke python startup method.
    Added Maya and Nuke python startup method.

--- core/Process.py
+++ core/Process.py
@@ -1,7 +1,7 @@
 
 import subprocess
 
-class Process(object):
+class Process():
 	"""Methodes for Process managment"""
 
 	@staticmethod
@@ -14,7 +14,10 @@ class Process(object):
 	@staticmethod
 	def launchSoftware( softwarePath, arg=None ):
 		if arg:
-			subprocess.Popen( [softwarePath, arg], creationflags=subprocess.CREATE_NEW_CONSOLE )
+			if not isinstance( arg, list):
+				arg = [arg]
+			subprocess.Popen( [softwarePath] + arg, creationflags=subprocess.CREATE_NEW_CONSOLE )
+
 		else:
 			subprocess.Popen( softwarePath, creationflags=subprocess.CREATE_NEW_CONSOLE )
 
@@ -22,3 +25,21 @@ class Process(object):
 	def partial( func, *args, **keywords ):
 		import functools
 		return functools.partial( func, *args, **keywords )
+
+	def launchMayaPython( self, cmd, arg='' ):
+
+		actions = '%s(%s)' %( cmd, arg ) 
+		cmdExec = 'exec(\'import sys\\\\nsys.path.append(\\\\\'F:/software/Forge_0.0.0.1dev/\\\\\')\\\\nsys.path.append(\\\\\'F:/software/Anvil_0.0.0.1dev/\\\\\')\\\\nsys.path.append(\\\\\'F:/software/hammer_0.0.0.1dev/\\\\\')\\\\nimport Hammer\\\\n' + actions + '\')'
+
+		self.launchSoftware( 'c:/Program Files/Autodesk/Maya2015/bin/maya.exe', arg=['-command', "python \"%s\";" %( cmdExec )] )
+
+	def launchNukePython( self, cmd, arg='' ):
+		path = 'f:/tmp/nukeStartupTmp.py'
+
+		actions = '%s(%s)' %( cmd, arg ) 
+		cmdExec = 'exec(\'import sys\\nsys.path.append(\\\'F:/software/Forge_0.0.0.1dev/\\\')\\nsys.path.append(\\\'F:/software/Anvil_0.0.0.1dev/\\\')\\nsys.path.append(\\\'F:/software/hammer_0.0.0.1dev/\\\')\\nimport Hammer\\n%s\\nimport Forge.core.System\\nForge.core.System.removeFile(\\\'%s\\\')\')' %( actions, path )
+
+		import Forge.core.System
+		Forge.core.System.setFile( path=path, content=cmdExec )
+
+		self.launchSoftware( 'c:/Program Files/Nuke8.0v3/Nuke8.0.exe', arg=path )
